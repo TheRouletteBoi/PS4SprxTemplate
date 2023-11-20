0291649939169bf17deb38f358544bf6340e5b28
TheRouletteBoi <therouletteboi@outlook.com>
2023-11-20 01:51:17 -0800

    Add Project solution

---
+++ PS4SprxTemplate/main.cpp
@@ -0,0 +1,18 @@
+#include <thread>
+#include <functional>
+#include <future>
+
+
+__BEGIN_DECLS
+int module_start(size_t args, const void* argp)
+{
+
+    return 0;
+}
+
+int module_stop(size_t args, const void* argp)
+{
+
+    return 0;
+}
+__END_DECLS
